186d0712474d4ccf076582b0e1230d6ddc25fa78
sujungs <138412245+sujungs@users.noreply.github.com>
2026-06-05 14:08:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -189,7 +189,7 @@ def normalize_eur(html):
 def catch_notice(url):
     # url 형식 예: "https://www.catch.co.kr/NCS/RecruitInfoDetails/539080"
     
-    req = requests.get("url", headers=headers)
+    req = requests.get(url, headers=headers)
     html = bs(req.text, "html.parser")
     
     # 기업명
